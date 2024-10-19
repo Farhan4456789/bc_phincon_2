df963c9a8be56b74f140c2c308772ac1412b4084
muhammadfarhanfadlil <farhanfadlil445@gmail.com>
2024-10-19 20:31:48 +0700

    Menambahkan file angka.js

--- angka.js
+++ angka.js
@@ -12,3 +12,4 @@ function bandingkan_angka(angka1, angka2) {
 console.log(bandingkan_angka(3, 5)); // Output: true
 console.log(bandingkan_angka(7, 2)); // Output: false
 console.log(bandingkan_angka(4, 4)); // Output: -1
+console.log();
